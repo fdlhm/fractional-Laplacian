ab093c333b255511e0af40e6969c06266818657f
Francisco de la Hoz <francisco.delahoz@ehu.eus>
2025-01-23 11:47:32 +0100

    fractionalLaplacianfunction.m

--- fractionalLaplacianfunction.m
+++ fractionalLaplacianfunction.m
@@ -18,11 +18,11 @@ utilde_=2*r*[u_(1:N);zeros((4*r-2)*N,1);u_(N+1:2*N)].*exp(1i*pi*[0:2*r*N-1 -2*r*
 % ik is the vector containing $ik$, with $k\in\{0,\ldots,2rN-1\}\cup\{-2rN,\ldots,-1\}$
 ik=1i*[0:2*r*N-1 -2*r*N:-1]';
 tildesn12=pi*((0:2*r*N-1)'+.5)/(2*r*N); % $\tilde s_{n+1/2}$
-ustildesn12=ifft(ik.*utilde_); % $u_s(\tilde s_{n+1/2})$
-usstildesn12=ifft(ik.^2.*utilde_); % $u_{ss}(\tilde s_{n+1/2})$
+Ustildesn12=ifft(ik.*utilde_); % $U_s(\tilde s_{n+1/2})$
+Usstildesn12=ifft(ik.^2.*utilde_); % $U_{ss}(\tilde s_{n+1/2})$
 % F is the vector storing the numerical approximations of
 % $f(s)=\sin(s)u_{ss}(s)+2\cos(s)u_{s}(s)$ at $s_j$
-F=sin(tildesn12).*usstildesn12(1:2*r*N)+2*cos(tildesn12).*ustildesn12(1:2*r*N);
+F=sin(tildesn12).*Usstildesn12(1:2*r*N)+2*cos(tildesn12).*Ustildesn12(1:2*r*N);
 I=singularintegral(N,r,a,1-a,F); % Invoke the function singularintegral
 % fraclapnum stores the numerical approximation of the fractional Laplacian of $u(x)$
 fraclapnum=(1/(L^a*2*gamma(2-a)*cos(pi*a/2)))*sin(sj).^(a-1).*I;
