4fd512fd3bfe7684b99d4ee08ccb4a0a806fd996
Francisco de la Hoz <francisco.delahoz@ehu.eus>
2026-04-06 09:30:24 +0200

    fractionalLaplacianexamplewithoutL.m

--- fractionalLaplacianexamplewithoutL.m
+++ fractionalLaplacianexamplewithoutL.m
@@ -1,7 +1,9 @@
 % fractionalLaplacianexamplewithoutL.m
 % If you use it, please cite the corresponding paper:
 % Jorge Cayama, Francisco de la Hoz, Carlota Mar\'{\i}a Cuesta, Carlos Javier Garc\'{\i}a-Cervera,
-% A fast convolution method for the fractional Laplacian in $\mathbb{R}$, (2025).
+% A fast convolution method for the fractional Laplacian in R,
+% Revista de la Real Academia de Ciencias Exactas, F\'{\i}sicas y Naturales. Serie A. Matem\'aticas 119:80 (2025), 1-45.
+% https://rdcu.be/eszTL
 %
 % This program approximates numerically the fractional Laplacian of 
 % $u(x)=(ix-1)/(ix+1)$. We work with $u(s)\equiv u(\cot(s))=e^{2is}$
